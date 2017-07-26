ae6b7233ab21fb0f62363e3196f52cdb1b2b65e3
lgentile06 <egentile@umich.edu>
2017-07-26 11:33:43 -0700

    ud

--- Maps Direction/AppDelegate.swift
+++ Maps Direction/AppDelegate.swift
@@ -1,10 +1,5 @@
 //
 //  AppDelegate.swift
-//  Maps Direction
-//
-//  Created by Agus Cahyono on 2/9/17.
-//  Copyright © 2017 balitax. All rights reserved.
-//
 
 import UIKit
 import GoogleMaps
--- Maps Direction/ViewController.swift
+++ Maps Direction/ViewController.swift
@@ -1,10 +1,6 @@
 //
 //  ViewController.swift
-//  Maps Direction
-//
-//  Created by Agus Cahyono on 2/9/17.
-//  Copyright © 2017 balitax. All rights reserved.
-//
+
 
 import UIKit
 import GoogleMaps
